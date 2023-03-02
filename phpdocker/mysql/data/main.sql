5fe6fcce84b1b8a0912200438cc1f4e6ef9abafd
SashaMelva <sa1524517@gmail.com>
2023-03-02 22:26:53 +0300

    assembling the project

---
+++ phpdocker/mysql/data/main.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users 
+(
+    user_id INT PRIMARY KEY AUTO_INCREMENT, 
+    login VARCHAR(20) NOT NULL, 
+    password VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE operations 
+(
+    operations_id INT PRIMARY KEY AUTO_INCREMENT, 
+    sum INT NOT NULL,
+    operations_type_id VARCHAR(100) NOT NULL, 
+    operations_user_id INT NOT NULL,
+    comment TEXT
+);
+CREATE TABLE types 
+(
+    type_id INT PRIMARY KEY AUTO_INCREMENT, 
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+ALTER TABLE operations ADD FOREIGN KEY (operations_user_id) REFERENCES users (user_id);
+ALTER TABLE operations ADD FOREIGN KEY (operations_type_id) REFERENCES types (type_id);
+
+INSERT INTO users (login, password) VALUES 
+            ('user', '123'), 
+            ('user2', '123'), 
+            ('user3', '123');
+
+INSERT INTO types (name, description) VALUES 
+            ('coming', ''), 
+            ('expense', '')
+
+
+INSERT INTO operations (sum, operations_type_id, operations_user_id, comment) VALUES 
+            ('300', '1', '1', ''), 
+            ('200', '1', '1', ''), 
+            ('1000', '2', '1', '');
+            
